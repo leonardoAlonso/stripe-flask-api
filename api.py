136dd34957f9f8b287e28a6cd83f79fb5d2f4b22
Leonardo Alonso <leonardoalonsososa@gmail.com>
2020-12-02 18:26:13 -0600

    :sparkles: Acept payments using stripe

--- api.py
+++ api.py
@@ -1,7 +1,13 @@
 from flask import Flask, request, jsonify
+from flask_cors import CORS
+
+import stripe
+
+stripe.api_key = 'secret'
 
 app = Flask(__name__)
 app.config["DEBUG"] = True
+CORS(app)
 
 
 @app.route('/', methods=['GET'])
@@ -11,7 +17,16 @@ def home():
 @app.route('/api/acept_payment', methods=['POST'])
 def acept_payment():
     data = request.get_json()
-    print(data)
-    return {'ff':'ff'}
+    amount = 2000
+    payment = stripe.PaymentIntent.create(
+        amount=amount*100,
+        currency='mxn',
+        payment_method=data['payment_method'],
+        payment_method_types=["card"],
+        off_session=True,
+        confirm=True
+    )
+    print(payment)
+    return payment
 
-app.run()
+app.run(ssl_context=('cert.pem', 'key.pem'))
